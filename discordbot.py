a14a2fe7b7287056cad795559c2cc6000b8c624f
mrkmmshr <160872109+mrkmmshr@users.noreply.github.com>
2024-02-23 22:32:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,10 @@ import discord
 import traceback
 from discord.ext import commands
 from os import getenv
+import requests
+
+# LINE Notifyのアクセストークン
+LINE_NOTIFY_ACCESS_TOKEN = 'elAVuc7T0cgDIMTtGJYH0nod7ipgnztlQm4Gb3wR5rv'
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,9 +22,17 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
+    # LINE Notifyに通知
+    message = f'**Discordサーバー:** {ctx.guild.name}\n' \
+              f'**チャンネル:** {ctx.channel.name}\n' \
+              f'**ユーザー:** {ctx.author.name}({ctx.author.id})\n' \
+              f'**コマンド:** /ping'
+    headers = {'Authorization': f'Bearer {LINE_NOTIFY_ACCESS_TOKEN}'}
+    data = {'message': message}
+    requests.post('https://notify-api.line.me/api/notify', headers=headers, data=data)
+
     await ctx.send('pong')
 
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
-
