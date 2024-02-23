e934e79ba79ebb1e394e64c821f3e2809234a2b8
mrkmmshr <160872109+mrkmmshr@users.noreply.github.com>
2024-02-23 23:19:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,10 @@ import discord
 import traceback
 from discord.ext import commands
 from os import getenv
+import requests
+
+# LINE Notifyのアクセストークン
+LINE_NOTIFY_ACCESS_TOKEN = 'YTrWPR8t1V7203V3Ok90AWcB8d1FgJ0dFVPj030lyxm'
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,9 +20,19 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    # 送信元チャンネルが特定のチャンネル以外の場合、処理をスキップ
+    if message.channel.id != CHANNEL_ID:
+        return
+
+    # メッセージ内容をLINE Notifyに送信
+    headers = {'Authorization': f'Bearer {LINE_NOTIFY_ACCESS_TOKEN}'}
+    data = {'message': message.content}
+    requests.post('https://notify-api.line.me/api/notify', headers=headers, data=data)
 
 
 token = getenv('DISCORD_BOT_TOKEN')
