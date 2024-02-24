bf8013f3a82fc057923efe19b10992495da5bdf4
mrkmmshr <160872109+mrkmmshr@users.noreply.github.com>
2024-02-24 11:52:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,5 @@
 import discord
 import traceback
-import requests
 from discord.ext import commands
 from os import getenv
 
@@ -9,12 +8,6 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-# LINE Notify にメッセージを送る関数
-def send_line_notify(message):
-    line_token = getenv('LINE_NOTIFY_TOKEN')
-    headers = {'Authorization': f'Bearer {line_token}'}
-    payload = {'message': message}
-    requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -22,15 +15,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.event
-async def on_message(message):
-    if 'スプラ' in message.content:
-        send_line_notify(f'Discord に「スプラ」が含まれるメッセージが投稿されました: {message.content}')
-    await bot.process_commands(message)
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
